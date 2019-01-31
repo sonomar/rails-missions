b2499683e5f9fb16e62110b69891d28a56cfd9e9
sonomar <lawsonmarlowe@gmail.com>
2019-01-31 17:59:43 -0500

    Worked through edge cases for Fibonacci sequence, controller is now writing to DB

--- app/controllers/fib_seq_controller.rb
+++ app/controllers/fib_seq_controller.rb
@@ -1,28 +1,56 @@
 class FibSeqController < ApplicationController
 	
 require 'benchmark'
+require 'httparty'
+require 'json'
 
-	def get_fib_value(series_num)
-			total = 1
-			first = 1
-			second = 1
+def get_fib_value(series_num)
+		series_num = series_num.to_i
+		total = 1
+		first = 1
+		second = 1
+		if series_num == 0
+			fib_data = {}
+			fib_data[:val] = 0
+			fib_data[:res] = 0
+			fib_data[:run] = 'NA'
+			return fib_data
+		elsif series_num == 1
+			fib_data = {}
+			fib_data[:val] = 1
+			fib_data[:res] = 1
+			fib_data[:run] = 'NA'
+			return fib_data	
+		else
 			time = Benchmark.measure {
-				(series_num -1).times do
+				(series_num-1).times do
 					total = first + second
 					first = second
 					second = total
 				end
 			}
 			fib_data = {}
-			fib_data[:series_number] = series_num
-			fib_data[:fib_result] = total
-			fib_data[:process_time_in_sec] = time.real.to_s 
+			fib_data[:val] = series_num
+			fib_data[:res] = total
+			fib_data[:run] = time.real.to_s 
 			return fib_data
-	end
+		end
+
+end
 
 	def index
-		@fibseq = FibSeq.all
-    end 
+    	@fibseq = FibSeq.limit(10).order("ID DESC").all
+    end
 
+    def create
+    	@seqnum = params[:seqnum]
+    	@sequence = get_fib_value(@seqnum) 
+    	newseq = FibSeq.create!(value: get_fib_value(@seqnum)[:val], result: get_fib_value(@seqnum)[:res], runtime: get_fib_value(@seqnum)[:run])
+    	if newseq.save
+    		render :json => @sequence
+    	else
+    		render json: {}, status: 400
+    	end  
+    end
 
 end
